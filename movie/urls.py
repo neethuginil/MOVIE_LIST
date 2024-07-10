b9116cda2a9105afa4420625af020a7d49e24860
neethu ginil <neethushalin.erezha@gmail.com>
2024-07-10 19:32:19 +0530

    Initial commit

---
+++ movie/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path('create/', views.create, name='create'),
+    path('edit/<pk>', views.edit, name='edit'),
+    path('delete/<pk>', views.delete, name='delete'),
+    path('', views.movielist, name='list'),
+]
